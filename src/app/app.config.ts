3dc840979163d8254fd6514e7fbbddfb51551606
Jonas <maximilian.jonas@dedalus.com>
2026-02-25 12:10:46 +0100

    adds some demo components that will require change after version update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {I18NewModule} from '@ucfw/i18n';
+import {provideHttpClient} from '@angular/common/http';
+import {provideAnimations} from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations(),
+    importProvidersFrom(I18NewModule.forRoot({defaults: {locale: 'de-DE'}}))
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,32 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {ButtonModule} from '@ucfw/components/button';
+import {CommonModule} from '@ucfw/common';
+import {AlertModule} from '@ucfw/components/alert';
+import {CalendarModule} from '@ucfw/components/calendar';
+import {FormsModule} from '@angular/forms';
+import {Mobiscroll4UModule} from '@ucfw/components/mbsc';
+import {TreeSelectElement, TreeSelectModule} from '@ucfw/components/tree-select';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, ButtonModule, AlertModule, CalendarModule, FormsModule, Mobiscroll4UModule, TreeSelectModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('migration-with-copilot');
+  protected date: Date = new Date();
+
+  protected readonly treeSelectData: TreeSelectElement<string>[] = [
+    {
+      type: 'select-category',
+      label: 'Fruits',
+      children: [
+        { type: 'select-item', label: 'Apple', value: 'apple' },
+        { type: 'select-item', label: 'Banana', value: 'banana' },
+        { type: 'select-item', label: 'Cherry', value: 'cherry' }
+      ]
+    }
+  ];
+
+  protected selection = [{ type: 'select-item', label: 'Apple', value: 'apple' },];
 }
